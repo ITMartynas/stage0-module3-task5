26a30114387058a2ee2687307e8ed400e9c48f05
Martynas <martynas.bartnykas@gmail.com>
2023-12-20 20:09:38 +0200

    Conducted simple tasks with variables.

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float secondNumber = 5.0F;
+        float result = toBeDivided/secondNumber;
+
+        System.out.println(result);
     }
 }
